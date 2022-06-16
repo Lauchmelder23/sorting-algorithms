d868ab0ac8b480c3f565fd36fcc8292d02dcf59e
Lauchmelder <robert.altner11@gmail.com>
2022-06-16 18:28:00 +0200

    cimgui is working

--- src/application.c
+++ src/application.c
@@ -6,6 +6,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
+#include <cimgui.h>
+#include <cimgui_impl.h>
+
 #include "visualizers/histogram.h"
 #include "algorithms.h"
 
@@ -44,6 +48,18 @@ int init_application(Application* app, int width, int height, const char* title)
 
     glViewport(0, 0, width, height);
 
+    igCreateContext(NULL);
+
+    ImGuiIO* io = igGetIO(); (void*)io;
+    io->ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io->ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    io->ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 460");
+
+    igStyleColorsDark(NULL);
+
     app->active_visualizer = &histogram_visualizer;
 
     visualizer_data.array_size = 1500;
@@ -68,6 +84,10 @@ int init_application(Application* app, int width, int height, const char* title)
 
 void destroy_application(Application* app)
 {
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    igDestroyContext(NULL);
+
     app->active_visualizer->exit();
 
     if(app->window)
@@ -79,9 +99,9 @@ void run_application(Application* app)
     GLFWwindow* window = app->window;
 
     // bubblesort(app);
-    // mergesort(app);
+    mergesort(app);
     // insertionsort(app);
-    shellsort(app);
+    // shellsort(app);
 
     while(!glfwWindowShouldClose(window))
     {
@@ -96,7 +116,29 @@ int update_screen(Application* app)
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    igNewFrame();
+
     app->active_visualizer->render();
+
+    if(igBegin("Test Window", NULL, 0))
+    {
+        igText("cImGui is working");
+
+        igEnd();
+    }
+
+    igRender();
+    ImGui_ImplOpenGL3_RenderDrawData(igGetDrawData());
+
+    if (igGetIO()->ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+	{
+		GLFWwindow* backup_current_context = glfwGetCurrentContext();
+		igUpdatePlatformWindows();
+		igRenderPlatformWindowsDefault(NULL, NULL);
+		glfwMakeContextCurrent(backup_current_context);
+	}
 
     glfwSwapBuffers(app->window);
     return glfwWindowShouldClose(app->window);
